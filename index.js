198eaeac689aa7c6286524179558b1ff3d0a5ef0
robcrockPI <robert@propellerindustries.com>
2020-10-31 14:37:28 -0300

    Added the CORSProxy.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
-axios.get('https://public.tableau.com/profile/api/following/will7508?count=12&index=0').then((res) => {
-  console.log(res.data)
-})
+const CORSProxy = 'https://cors-anywhere.herokuapp.com/'
+const baseEndpoint = 'https://public.tableau.com/profile/api/';
 
-console.log('Hello from JS 👋');
+axios.get(`${CORSProxy}${baseEndpoint}will7508`).then((res) => {
+  console.log(res.data.workbooks)
+})
